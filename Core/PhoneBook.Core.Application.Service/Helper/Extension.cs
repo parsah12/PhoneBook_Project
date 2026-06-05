e6ec667d3ec1354db107b5670c38adf5d8763ddf
parsah12 <hedayati82parsa@gmail.com>
2026-06-05 15:04:04 +0330

    add extension for performance

---
+++ Core/PhoneBook.Core.Application.Service/Helper/Extension.cs
@@ -0,0 +1,18 @@
+﻿using PhoneBook.Core.Application.Dto;
+using PhoneBook.Core.Domain.Entities;
+
+namespace PhoneBook.Core.Application.Service.Helper;
+
+public static class Extension
+{
+    public static ContactDto ToDto(this ContactEntity entity)
+    {
+        return new ContactDto
+        {
+            FirstName = entity.FirstName,
+            LastName = entity.LastName,
+            PhoneNumber = entity.PhoneNumber,
+            Tag = entity.Tag
+        };
+    }
+}

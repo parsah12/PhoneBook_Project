3c56ddb5ba2f5779a0d51dca4b050f9a6a8f3187
parsah12 <hedayati82parsa@gmail.com>
2026-06-05 14:55:44 +0330

    add new methods for PhoneBookService

--- Core/PhoneBook.Core.Application.Service/Service/ContactService.cs
+++ Core/PhoneBook.Core.Application.Service/Service/ContactService.cs
@@ -5,27 +5,27 @@ namespace PhoneBook.Core.Application.Service.Service;
 
 public class ContactService : IContactService
 {
-    public Task<ContactDto> AddNewContact(string firstName, string lastName, string phoneNum, string tag)
+    public Task<ContactDto> AddNewContactAsync(ContactDto contact)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ContactDto> DeleteContact(string firstName, string lastName, string phoneNum, string tag)
+    public Task DeleteContactAsync(int id)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ContactDto> GetAllContact()
+    public Task<List<ContactDto>> GetAllContactsAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task<ContactDto> GetContactBuyTag(string tag)
+    public Task<List<ContactDto>> GetContactByTagAsync(string tag)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ContactDto> UpdateContact(string firstName, string lastName, string phoneNum, string tag)
+    public Task<ContactDto> UpdateContactAsync(int id, ContactDto contact)
     {
         throw new NotImplementedException();
     }
--- Core/PhoneBook.Core.Application/IService/IContactService.cs
+++ Core/PhoneBook.Core.Application/IService/IContactService.cs
@@ -4,9 +4,13 @@ namespace PhoneBook.Core.Application.IService;
 
 public interface IContactService
 {
-    Task<ContactDto> AddNewContact(string firstName, string lastName, string phoneNum, string tag);
-    Task<ContactDto> UpdateContact(string firstName, string lastName, string phoneNum, string tag);
-    Task<ContactDto> DeleteContact(string firstName, string lastName, string phoneNum, string tag);
-    Task<ContactDto> GetAllContact();
-    Task<ContactDto> GetContactBuyTag(string tag);
+    Task<ContactDto> AddNewContactAsync(ContactDto contact);
+
+    Task<ContactDto> UpdateContactAsync(int id,ContactDto contact);
+
+    Task DeleteContactAsync(int id);
+
+    Task<List<ContactDto>> GetAllContactsAsync();
+
+    Task<List<ContactDto>> GetContactByTagAsync(string tag);
 }
